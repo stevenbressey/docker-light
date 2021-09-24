6e1f28ae89f956b0ecc3a68b39eec82eea09fabe
Steven Bressey <steven.bressey@artifakt.io>
2021-09-24 17:09:49 +0200

    hostname

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func main() {
@@ -12,4 +13,5 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, os.Getenv("HOSTNAME"))
 }
